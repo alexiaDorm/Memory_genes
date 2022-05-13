2a918a3a3c07a414d65cba52531489ef9842cf69
Alexia D <alexia.dormann@epfl.ch>
2022-05-13 22:38:02 +0200

    poj

--- src/ADAboostFused.py
+++ src/ADAboostFused.py
@@ -77,11 +77,11 @@ y = np.array(fused['memory_gene'])
 
 #Adaboosting with decision trees - hyperparameters tuning
 #Define model, grid search space, CV
-for i in range(3,10):
+for i in range(8,21):
     print(i)
     base_tree = DecisionTreeClassifier(max_depth = i, class_weight = 'balanced')
     model = AdaBoostClassifier(base_estimator = base_tree)
-    grid = {'n_estimators' : [50, 100, 500],'learning_rate' : [0.01, 0.1, 1.0]}
+    grid = {'n_estimators' : [300, 400, 500],'learning_rate' : [0.01, 0.1, 1.0]}
     cv = KFold(n_splits=5, shuffle=True, random_state=1)
     grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=cv, scoring='accuracy')
     
