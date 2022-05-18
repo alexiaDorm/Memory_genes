66b64b9d3ff1fb2b4b11c5df8cb87f61a4ebdacb
Alexia D <alexia.dormann@epfl.ch>
2022-05-18 11:21:48 +0200

    eg

--- src/LogRegFused.py
+++ src/LogRegFused.py
@@ -77,7 +77,7 @@ y = fused['memory_gene']
 #Select relevant features
 best_acc, best_param, best_feat = 0, 0, []
 for i in range (2, X.shape[1] + 1):
-    selector = SelectKBest(mutual_info_classif, k=i)
+    selector = SelectKBest(f_classif, k=i)
     X_redu = selector.fit_transform(X, y)
 
     #Grid search around best found parameters during random grid search
@@ -109,4 +109,4 @@ for i in data_to_fuse:
     
 #Save individual clustering results
 scores_df = pd.DataFrame(clust_score, index = name_fused, columns= ['precision', 'recovery','100 precision', '100 recovery'])
-scores_df.to_csv('../data/binaryClass_scores/LogFS_MI.csv', index=True)
+scores_df.to_csv('../data/binaryClass_scores/LogFS_F.csv', index=True)
