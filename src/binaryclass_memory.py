ef20dc1625a5516b944c87c55a5f623033c01ff4
Alexia D <alexia.dormann@epfl.ch>
2022-05-25 11:58:16 +0200

    NN1l

--- src/binaryclass_memory.py
+++ src/binaryclass_memory.py
@@ -369,7 +369,7 @@ def obj(trial, fused):
     params = {
               'learning_rate': trial.suggest_loguniform('learning_rate', 1e-8, 100),
               'weight_decay' : trial.suggest_loguniform('weight_decay', 1e-5, 1),
-              'n1': trial.suggest_int("n1", 4, 50),
+              #'n1': trial.suggest_int("n1", 4, 50),
               #'n2' : trial.suggest_int("n2", 4, 50), 
               #'n3' : trial.suggest_int("n3", 4, 50),
               #'batch_size': trial.suggest_int("batch_size", 5, 8), #2^i
@@ -387,7 +387,7 @@ def obj(trial, fused):
 
     train_dl, test_dl = load_data(fused[FS],params)
 
-    model = NN_3l(len(FS)-1, params)
+    model = NN_1l(len(FS)-1, params)
 
     #Optmization criterion and optimizer
     num_positives= np.sum(y); num_negatives = len(y) - num_positives
