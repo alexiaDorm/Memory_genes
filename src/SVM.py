a0965d5ceb6cd724e2ff2fffbe3ee4312a9a89c6
Alexia D <alexia.dormann@epfl.ch>
2022-06-01 11:28:21 +0200

    SVM + clean

--- src/SVM.py
+++ src/SVM.py
@@ -6,6 +6,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import sklearn
 import pyreadr
+from sklearn.svm import SVC
 
 from load_data import open_charac
 from binaryclass_memory import *
@@ -21,8 +22,9 @@ X = fused[['CV2ofmeans_residuals','mean_expression']]
 y = fused['memory_gene']     
 
 #Grid search around best found parameters during random grid search
-model = LogisticRegression(class_weight = "balanced_subsample")
-grid = {'C': np.logspace(-5, 5, 11)}
+model = SVC(kernel='rbf', class_weight='balanced', random_state=1)
+grid = {'C': np.logspace(-5, 5, 11),
+        'gamma': ['scale', 'auto']}
 
 cv = KFold(n_splits=5, shuffle=True, random_state=1)
 grid_search = GridSearchCV(estimator=model, param_grid=grid, cv=cv, scoring='balanced_accuracy')
