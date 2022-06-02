669a680952467991268c18457b7486e32280bb7c
Alexia D <alexia.dormann@epfl.ch>
2022-06-02 21:47:02 +0200

    Update ValidationNN.py

--- src/ValidationNN.py
+++ src/ValidationNN.py
@@ -61,8 +61,8 @@ for i in val:
     recovery, false_pos = compute_enrichment(charac_matrix[i], y, yhat)
     score = [acc, recovery, false_pos]
     
-    score.extend(predict_evaluate_NN(charac_matrix[i].index, yhat, norm_matrix[i], families_matrix[i], mult_pred=True, outliers = outliers[i]))
+    score.extend(predict_evaluate(charac_matrix[i].index, yhat, norm_matrix[i], families_matrix[i], mult_pred=True, outliers = outliers[i]))
     scores.append(score)
     
 scores_df = pd.DataFrame(scores, index = names_fused + names_val, columns= ['accuracy', 'recovery memory gene', 'FP', 'precision', 'recovery', 'ensembling precision', 'ensembling recovery'])
-scores_df.to_csv('../data/binaryClass_scores/bestNNpos3.csv', index=True)
+scores_df.to_csv('../data/binaryClass_scores/bestNNpos5.csv', index=True)
