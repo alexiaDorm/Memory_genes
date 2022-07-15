f1a2e1374fbdf1dcc679ccd45f6ece0f5ddc1c68
Alexia D <alexia.dormann@epfl.ch>
2022-07-15 12:44:52 +0200

    ll

--- src/binaryclass_memory.py
+++ src/binaryclass_memory.py
@@ -348,12 +348,12 @@ def obj(trial, fused):
               'weight_decay' : trial.suggest_loguniform('weight_decay', 1e-10, 1e-5),
               'n1': trial.suggest_int("n1", 20, 50),
               'n2' : trial.suggest_int("n2", 20, 50), 
-              #'n3' : trial.suggest_int("n3", 15, 40),
+              'n3' : trial.suggest_int("n3", 15, 40),
               }
     #Load data
     train_dl, test_dl = load_data(fused,params)
 
-    model = NN_3lRBN(2, params)
+    model = NN_4lRBN(2, params)
 
     #Optmization loss and optimizer
     pos_weight = torch.as_tensor(4., dtype=torch.float)
