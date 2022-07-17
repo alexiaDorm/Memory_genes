f5f91b06c7f8ecce00cfebd3832cca9f3c7b6f0f
Alexia D <alexia.dormann@epfl.ch>
2022-07-17 15:02:29 +0200

    Update ValidationNN.py

--- src/ValidationNN.py
+++ src/ValidationNN.py
@@ -55,7 +55,7 @@ for i in val:
     recovery, false_pos = compute_enrichment(charac_matrix[i], y, yhat)
     score = [acc, recovery, false_pos]
     
-    score.extend(predict_evaluate(charac_matrix[i].index, yhat, norm_matrix[i], families_matrix[i], mult_pred=True)
+    score.extend(predict_evaluate(charac_matrix[i].index, yhat, norm_matrix[i], families_matrix[i], mult_pred=True))
     scores.append(score)
     
 scores_df = pd.DataFrame(scores, index = names_fused + names_val, columns= ['accuracy', 'recovery memory gene', 'FP', 'precision', 'recovery', 'ensembling precision', 'ensembling recovery'])
